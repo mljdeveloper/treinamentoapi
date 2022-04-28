99f94bacec8c2a1f791ed4bf69b2086d106eac26
marcosjesus <marcos.luiz.jesus@hotmail.com>
2022-04-28 15:57:08 -0400

    novos

--- treinamentoapi/settings.py
+++ treinamentoapi/settings.py
@@ -147,12 +147,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-django_heroku.settings(locals())
