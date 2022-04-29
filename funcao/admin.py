f773f5494dd20362d1b32a708639bb8d465aa920
marcosjesus <marcos.luiz.jesus@hotmail.com>
2022-04-29 06:55:40 -0400

    added admin funcao

--- funcao/admin.py
+++ funcao/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Funcao
 
 # Register your models here.
+
+
+class FuncaoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nomefuncao', 'create_at', 'updated_at', 'mostrar')
+    list_display_links = ('id', 'nomecargo')
+    list_per_page = 200
+    search_fields = ('id', 'nomefuncao',)
+    list_editable = ('mostrar',)
+    exclude = ['username', 'superusuario', ]
+
+    def get_queryset(self, request):
+        qs = super(FuncaoAdmin, self).get_queryset(request)
+        return qs.filter(username=request.user)
+
+    def save_model(self, request, obj, form, change):
+        obj.username = request.user
+        obj.superusuario = request.user
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(Funcao, FuncaoAdmin)
