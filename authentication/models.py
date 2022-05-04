315640112d9ba5eb9d458624ff4f8ece92cfac54
marcosjesus <marcos.luiz.jesus@hotmail.com>
2022-05-04 09:24:35 -0400

    login ok

--- authentication/models.py
+++ authentication/models.py
@@ -101,6 +101,6 @@ class User(AbstractBaseUser, PermissionsMixin, TrackingModel):
     def token(self):
         token = jwt.encode({'username': self.username,
                             'email': self.email,
-                            'exp': datetime.utcnow() + timedelta(minute=10)},
+                            'exp': datetime.utcnow() + timedelta(hours=24)},
                            settings.SECRET_KEY, algorithm='HS256')
         return token
--- basic.py
+++ basic.py
@@ -23,19 +23,18 @@ get_response = requests.post(url='http://127.0.0.1:8080/api/funcao/',
 print(get_response.json())
 """
 
-
+"""
 hearder = {"Authorization": "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6Im1hcmNvcyIsImVtYWlsIjoibWFyY29zLmx1aXouamVzdXNAaG90bWFpbC5jb20iLCJleHAiOjE2ODMwNTYwOTd9.I9HOKQ2ph_baHosfB185LsJosSuvTAD5rvb_2Tk8JnQ"}
-dados = {"nomefuncao": "pedreiro"}
-get_response = requests.get(url='http://127.0.0.1:8080/api/funcao/?id=1',
-                            headers=hearder
+#dados = {"nomefuncao": "pedreiro"}
+#get_response = requests.get(url='http://127.0.0.1:8080/api/funcao/?id=1',
+#                            headers=hearder
                             )
 print(get_response.json()['results'][-1]['nomefuncao'])
-
-
 """
+
+
 usuario = requests.post('http://localhost:8080/api/auth/login', {
     'email': "marcos.luiz.jesus@hotmail.com",
     'password': "10102020"
 })
 print(usuario.json())
-"""
