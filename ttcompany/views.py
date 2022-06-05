df98624286a741542741f723da88f8169f2b0e1a
marcosjesus <marcos.luiz.jesus@hotmail.com>
2022-06-05 16:03:52 -0400

    filter company by username

--- ttcompany/views.py
+++ ttcompany/views.py
@@ -5,20 +5,26 @@ from rest_framework.parsers import MultiPartParser, FormParser
 from django.shortcuts import get_object_or_404
 from authentication.models import User
 from django.contrib.auth import authenticate
-from rest_framework import response, status, permissions
+from rest_framework import response, status, permissions, filters
 from authentication.serializers import LoginSerializer, RegisterSerializer
 from django.shortcuts import render
 from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CreateCompanyAPIView(APIView):
     permission_classes = (IsAuthenticated,)
     parser_classes = [MultiPartParser, FormParser]
 
+    filter_backends = [DjangoFilterBackend,
+                       filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['username']
+    search_fields = ['username']
+    ordering_fields = ['username']
+
     def post(self, request, format=None):
-        print(request.data)
         serializer = TTCompanySerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -32,7 +38,7 @@ class CompanyDetailAPIView(RetrieveUpdateDestroyAPIView):
 
     permission_classes = (IsAuthenticated,)
 
-    lookup_field = "id"
+    lookup_field = "username"
 
     def get_queryset(self):
         return TTCompany.objects.filter(username=self.request.user)
