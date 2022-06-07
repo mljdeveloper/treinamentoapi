1c4589142661dcccacd4dbe0b66b790ac9828073
marcosjesus <marcos.luiz.jesus@hotmail.com>
2022-06-06 23:30:50 -0400

    fixed owner app

--- ttunit/serializers.py
+++ ttunit/serializers.py
@@ -17,13 +17,15 @@ class TTownerSerializers(ModelSerializer):
 
 class TTUnitSerializer(ModelSerializer):
 
-    ttowner = TTownerSerializers(many=True, read_only=True)
+    #ttowner = TTownerSerializers(many=True, read_only=True)
 
     class Meta:
         model = TTUnit
         fields = ('id', 'unittype', 'company',
                   'broker', 'status', 'active', 'businessdate',
-                  'price', 'modal', 'bedroom', 'restrooom', 'petpolicy', 'owner', 'ttowner',
+                  'price', 'modal', 'bedroom', 'restrooom', 'petpolicy',
+                  # 'owner',
+                  # 'ttowner',
                   'tabela')
 
     read_only_fields = ['tabela']
