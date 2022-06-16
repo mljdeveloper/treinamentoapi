fad2287797baa6f7c664ba3927d854bcf061b9f0
marcosjesus <marcos.luiz.jesus@hotmail.com>
2022-06-16 14:34:50 -0400

    testete

--- treinamentoapi/settings.py
+++ treinamentoapi/settings.py
@@ -63,10 +63,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -109,20 +109,13 @@ DATABASES = {
     'default': dj_database_url.config()
 }
 
-CORS_ORIGIN_ALLOW_ALL = False
 
-CORS_ORIGIN_WHITELIST = (
-    "http://localhost:8080",
-    "http://localhost:8080/",
-    "https://tecnotitan.herokuapp.com"
-)
-"""
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
-    "http://localhost:8080/",
-    "https://tecnotitan.herokuapp.com"
+    "https://gestaotreinamento.herokuapp.com",
+    "https://tecnotitan.herokuapp.com",
+    "https://62a27afe5121e8282a30b0b0--chipper-marigold-942594.netlify.app"
 ]
-"""
 
 CORS_ALLOW_METHODS = (
     'GET',
