2e975f1cc23b0e463e54dd77db0c35150af3d94a
marcosjesus <marcos.luiz.jesus@hotmail.com>
2022-06-16 13:50:39 -0400

    testes

--- ttunit/urls.py
+++ ttunit/urls.py
@@ -1,8 +1,9 @@
-from ttunit.views import CreateUnitAPIView, TTUnitDetailAPIView
-from django.urls import path
+from ttunit.views import TTUnitList, CreateUnitAPIView, TTUnitDetailAPIView
+from django.urls import path, re_path
 
 urlpatterns = [
     path("", CreateUnitAPIView.as_view(), name="unit"),
     path("<int:id>", TTUnitDetailAPIView.as_view(), name="units"),
+    re_path('^lista/(?P<id>.+)/$', TTUnitList.as_view()),
 
 ]
