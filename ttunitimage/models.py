dab316f54e871c7dd9d88ad6c7dca1524f14619b
marcosjesus <marcos.luiz.jesus@hotmail.com>
2022-06-17 18:04:20 -0400

    testet

--- ttunitimage/models.py
+++ ttunitimage/models.py
@@ -4,14 +4,18 @@ from helpers.models import TrackingModel
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from authentication.models import User
-# Create your models here.
+from django.db.models import signals
+from django.template.defaultfilters import slugify
+import random
+# Create your model
 
 
 def upload_to(instance, filename):
-    return '/'.join(['units', str(instance.name), filename])
+    return '/'.join(['units', str(instance.tabela), filename])
 
 
 class TTUnitImage(TrackingModel):
+    slug = models.SlugField('Atalho', null=True, unique=True)
     unit = models.ForeignKey(to=TTUnit, on_delete=models.CASCADE,
                              related_name='ttunit_image', null=True, blank=True)
     photo = models.ImageField(
@@ -28,9 +32,16 @@ class TTUnitImage(TrackingModel):
         return valor
 
     def __str__(self):
-        return self.unit
+        return self.slug
 
     class Meta:
         ordering = ('unit',)
         verbose_name = 'Image'
         verbose_name_plural = 'Images'
+
+
+def ttunitimage_pre_save(signal, instance, sender, **kwargs):
+    instance.slug = slugify(random.randrange(1, 10000000000, 3))
+
+
+signals.pre_save.connect(ttunitimage_pre_save, sender=TTUnitImage)
